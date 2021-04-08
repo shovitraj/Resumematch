2194f7c9852d55d4f8f3befc232eef5a0de97a09
shovitraj <shovitraj@gmail.com>
2021-04-08 12:28:09 -0700

    components added to app.py

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 from nlp import preprocess
 from nlp import Similarity
+from Footer import Footer
 import streamlit as st 
 import numpy as np
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+Footer()
 
-st.title('Resume Match')
+JOB_DEF= 'Paste your job description here.'
+RES_DEF ='Paste your resume here.'
+# hide_streamlit_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-job = st.text_area('Paste your Job Description')
-resume = st.text_area('Paste your Resume')
+st.sidebar.title('Resume Match')
+
+st.header('Match your Resume with any Job Description')
+
+job = st.text_area('Job Description', JOB_DEF, height=350)
+resume = st.text_area('Resume', RES_DEF, height=350)
 # job = st.file_uploader('Upload Job Descripltion')
 
 job = preprocess(job)
@@ -22,8 +29,14 @@ resume = preprocess(resume)
 
 matchPercentage = np.round((Similarity(job, resume)*100),2)
 
-if len(job) != 0 and len(resume) != 0:
-    st.write("Your Resume matched", matchPercentage, '% with the job description')
+if len(job) >= 50 and len(resume) >= 50:
+    if len(job) >=50 and len(resume) >=50:
+        st.sidebar.markdown(
+        f'<div style="color: green; font-size: largest"> Your resume matched <h1> {matchPercentage}% </h1> with the job description. </h1></div>',
+        unsafe_allow_html=True)
+#         st.write("Your Resume matched", matchPercentage, '% with the job description')
+        if matchPercentage >= 80:
+            st.balloons()
 
     
 
